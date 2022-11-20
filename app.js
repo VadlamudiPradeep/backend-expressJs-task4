2d53624ade9f467ba5534ca8a8f7684299dcc236
vadlamudi pradeep <vandlamudipradeepchoudhary@gmail.com>
2022-11-20 11:55:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended : false}))
+
+app.use('/admin',adminRouter);
+app.use(shopRouter);
+
+
+app.use('/',(req , res ,next)=>{
+    res.status(404).send("<h1>Page is not found <h1>")
+})
+
+app.listen(3000);
